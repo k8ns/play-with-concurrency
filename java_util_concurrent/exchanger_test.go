39cc7cfc4d90aec117a5af6df18f932c773b9d7d
Konstantin Sopin <sopinorg@gmail.com>
2018-10-15 20:51:18 +0100

    mv exchanger demo to test

---
+++ java_util_concurrent/exchanger_test.go
@@ -0,0 +1,36 @@
+package java_util_concurrent
+
+import (
+	"fmt"
+	"sync"
+)
+
+func ExampleStringExchanger() {
+
+	exch := NewExchanger()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		s := exch.Exchange("one")
+		fmt.Println("one got", s)
+		wg.Done()
+	}()
+
+	go func() {
+		s := exch.Exchange("two")
+		fmt.Println("two got", s)
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	//Unordered output:
+	//one got two
+	//two got one
+}
+
+
+
+
+
